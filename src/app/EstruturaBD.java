8a43b259bdc3414e7471dd5961a244b567320382
Fernanda Bernardo <fernandabernardo94@gmail.com>
2013-11-25 19:43:06 -0200

    lendo as estruturas para arraylist

--- src/app/EstruturaBD.java
+++ src/app/EstruturaBD.java
@@ -1,6 +1,20 @@
 package app;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Scanner;
 
 public class EstruturaBD {
-
+	ArrayList<String> bd;
+	private Scanner sc;
+	
+	public EstruturaBD() throws FileNotFoundException {
+		bd = new ArrayList<>();
+		
+		sc = new Scanner(new File("src/banco/bd.txt"));
+		while (sc.hasNext()) {
+			bd.add(sc.nextLine());
+		}
+	}
 }
---
+++ src/app/MainThreads.java
@@ -0,0 +1,10 @@
+package app;
+
+import java.io.FileNotFoundException;
+
+public class MainThreads {
+
+	public static void main(String[] args) throws FileNotFoundException {
+		EstruturaBD bd = new EstruturaBD();
+	}
+}
